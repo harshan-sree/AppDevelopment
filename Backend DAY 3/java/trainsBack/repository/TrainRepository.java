071d6e1f5259e6decaf58c97db64d3b47737f028
harshan-sree <144901898+harshan-sree@users.noreply.github.com>
2023-10-05 12:46:07 +0530

    Delete Backend DAY 3 directory

--- Backend DAY 3/java/trainsBack/repository/TrainRepository.java
+++
@@ -1,9 +0,0 @@
-package trainsBack.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import trainsBack.model.Train;
-
-public interface TrainRepository extends JpaRepository<Train,Long>{
-
-}
